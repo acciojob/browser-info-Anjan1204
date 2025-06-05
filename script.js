528e81fc41443458961f05066ac5c256e4c0e000
Anjani Kumar <anjan.1204kr@gmail.com>
2025-06-05 13:39:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+function getBrowserInfo() {
+  const userAgent = navigator.userAgent;
+  let browserName = navigator.appName;
+  let version = navigator.appVersion;
+
+  // Browser name detection logic
+  if (userAgent.includes("Chrome") && !userAgent.includes("Edge") && !userAgent.includes("OPR")) {
+    browserName = "Google Chrome";
+    version = userAgent.match(/Chrome\/([\d.]+)/)[1];
+  } else if (userAgent.includes("Firefox")) {
+    browserName = "Mozilla Firefox";
+    version = userAgent.match(/Firefox\/([\d.]+)/)[1];
+  } else if (userAgent.includes("Safari") && !userAgent.includes("Chrome")) {
+    browserName = "Safari";
+    version = userAgent.match(/Version\/([\d.]+)/)[1];
+  } else if (userAgent.includes("Edg")) {
+    browserName = "Microsoft Edge";
+    version = userAgent.match(/Edg\/([\d.]+)/)[1];
+  } else if (userAgent.includes("OPR") || userAgent.includes("Opera")) {
+    browserName = "Opera";
+    version = userAgent.match(/(Opera|OPR)\/([\d.]+)/)[2];
+  }
+
+  document.getElementById("browser-info").textContent =
+    "You are using " + browserName + " version " + version;
+}
+
+getBrowserInfo();
